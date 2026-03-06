6e7e2096b7095cf7ce5e80bcfff4efa6994fb5d3
Somotosh <somotosh212@gmail.com>
2026-03-06 22:24:55 +0600

    added HTML staracer

---
+++ script/login.js
@@ -0,0 +1,11 @@
+const loginBtn =document.getElementById("login-btn");
+
+loginBtn.addEventListener("click",()=>{
+    const inputUsername =document.getElementById("input-username");
+    const username =inputUsername.value
+    console.log(username)
+    const inputPassword =document.getElementById("input-password");
+    const password =inputPassword.value
+    console.log(password)
+    if(username =="")
+})
